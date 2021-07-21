1cdea26648d6eb1ba95715c69d749335f4d23523
vitordiniz25 <vitordiniz25@gmail.com>
2021-07-21 20:14:00 -0300

    Component Content

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,7 +6,7 @@ export const GlobalStyle = createGlobalStyle`
         --background: #f0f2f5;
         --red: #E52E4D;
         --blue: #5429CC;
-
+        --green: #33CC95;
         --blue-light: #6933FF;
 
         --text-title: #363f5F;
@@ -32,7 +32,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body {
-        background: var(---background);
+        background: var(--background);
         -webkit-font-smoothing: antialiased;
     }
 
