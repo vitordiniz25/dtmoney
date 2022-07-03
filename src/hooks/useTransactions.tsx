29b44b671ca0948847ca48a9822609b23e96373c
vitordiniz25 <vitordiniz25@gmail.com>
2022-07-03 20:48:18 -0300

    terminando cap2

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,11 @@
-import { createContext, ReactNode, useEffect, useState } from "react";
-import { api } from "./services/api";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { api } from "../services/api";
 
 interface Transaction {
   id: number;
@@ -18,10 +24,10 @@ type TransactionInput = Omit<Transaction, "id" | "createdAt">;
 
 interface TransactionContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionContextData>(
+const TransactionsContext = createContext<TransactionContextData>(
   {} as TransactionContextData
 );
 
@@ -34,8 +40,14 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       .then((response) => setTransactions(response.data.transactions));
   }, []);
 
-  function createTransaction(transaction: TransactionInput) {
-    api.post("/transactions", transaction);
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post("/transactions", {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
+    const { transaction } = response.data;
+
+    setTransactions([...transactions, transaction]);
   }
 
   return (
@@ -43,4 +55,10 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       {children}
     </TransactionsContext.Provider>
   );
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+
+  return context;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,54 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
-import { createServer, Model } from 'miragejs';
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+import { createServer, Model } from "miragejs";
 
 createServer({
   models: {
     transaction: Model,
   },
 
-  seeds(server){
+  seeds(server) {
     server.db.loadData({
       transactions: [
         {
           id: 1,
-          title: 'Freelance de website',
-          type: 'deposit',
-          category: 'Dev',
+          title: "Freelance de website",
+          type: "deposit",
+          category: "Dev",
           amount: 6000,
-          createdAt: new Date('2021-02-12 09:00:00')
+          createdAt: new Date("2021-02-12 09:00:00"),
         },
         {
           id: 2,
-          title: 'Aluguel',
-          type: 'withdraw',
-          category: 'Casa',
-          amount: 1000,
-          createdAt: new Date('2021-02-14 11:00:00')
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1100,
+          createdAt: new Date("2021-02-14 11:00:00"),
         },
-      ]
-    })
+      ],
+    });
   },
 
   routes() {
-    this.namespace = 'api';
+    this.namespace = "api";
 
-    this.get('/transactions', () => {
-      return this.schema.all('transaction')
-    })
+    this.get("/transactions", () => {
+      return this.schema.all("transaction");
+    });
 
-    this.post('/transactions', (schema, request) => {
+    this.post("/transactions", (schema, request) => {
       const data = JSON.parse(request.requestBody);
 
-      return schema.create('transaction', data);
-    })
-  }
-})
+      return schema.create("transaction", data);
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
